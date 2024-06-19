12e3d797a2c9870f440644d87b0d968d34395de7
AntonIX <antonix@devpins.org>
2024-06-20 01:31:47 +0300

    Fix sync

--- device/xiaomi-miatoll/env.sh
+++ device/xiaomi-miatoll/env.sh
@@ -10,15 +10,14 @@ SOC=qcom
 PORT_TYPE=hybris
 HYBRIS_VER=18.1
 ANDROID_MAJOR_VERSION=11
-#REPO_INIT_URL="https://github.com/mer-hybris/android.git"
-#REPO_LOCAL_MANIFESTS_URL=""
-#HAL_MAKE_TARGETS=(hybris-hal droidmedia hwcrypt bzip2 libbiometry_fp_api)
-#HAL_ENV_EXTRA=""
+REPO_INIT_URL="https://github.com/SailfishOS-miatoll/android.git"
+REPO_LOCAL_MANIFESTS_URL="https://github.com/SailfishOS-miatoll/local_manifests.git"
+HAL_MAKE_TARGETS=(hybris-hal droidmedia hwcrypt bzip2 libbiometry_fp_api)
+HAL_ENV_EXTRA="USE_CCACHE=1"
 RELEASE=4.5.0.25
 TOOLING_RELEASE=4.5.0.18
 SDK_RELEASE=latest
 REPOS=(
-    'https://github.com/SailfishOS-miatoll/local_manifests.git' .repo/local_manifests "master" 1
     'https://bitbucket.org/thexperienceproject/yuki-clang.git' prebuilts/yuki-clang "18.0.0" 1
 )
 #LINKS=()
