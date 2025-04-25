9262b7e3ef19598cba0a6a1560f7bdb2a2b4887c
AntonIX <antonix@devpins.org>
2025-04-25 23:20:15 +0300

    update

--- device/xiaomi-miatoll/env.sh
+++ device/xiaomi-miatoll/env.sh
@@ -14,12 +14,12 @@ REPO_INIT_URL="https://github.com/SailfishOS-miatoll/android.git"
 REPO_LOCAL_MANIFESTS_URL="https://github.com/SailfishOS-miatoll/local_manifests.git"
 HAL_MAKE_TARGETS=(hybris-hal droidmedia bzip2 libbiometry_fp_api)
 HAL_ENV_EXTRA=""
-RELEASE=4.6.0.13
-TOOLING_RELEASE=4.6.0.11
+RELEASE=5.0.0.67
+TOOLING_RELEASE=5.0.0.62
 SDK_RELEASE=latest
 REPOS=(
-    'https://bitbucket.org/thexperienceproject/yuki-clang.git' prebuilts/yuki-clang "19.0.0git" 1
-    'https://github.com/libhybris/libhybris.git' hybris/mw/libhybris "master" 1
+    'https://bitbucket.org/thexperienceproject/yuki-clang.git' prebuilts/yuki-clang "18.0.0" 1
+    'https://github.com/mer-hybris/libhybris.git' hybris/mw/libhybris "master" 1
 )
 #LINKS=()
 export VENDOR DEVICE PORT_ARCH RELEASE
